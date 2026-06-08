188d351b5e188d7ee038239e5d395d212b8e8429
Iliyana KEHAYOVA <iliyana.kehayova2407@gmail.com>
2026-06-08 10:26:15 +0200

    exo js

--- js/machine.mjs
+++ js/machine.mjs
@@ -4,49 +4,70 @@ async function main(){
 const sc = new createInterface({input, output});
 
 let qteGobelets = 63;
-let produit;
+let montant;
+let choixBoisson;
+
+
 
 const prix = new Map();
-prix.set("1", 'Choix n°1. Café : 1,20 euros');
-prix.set("2", 'Choix n°2. Thé : 1,10 euros');
-prix.set("3", 'Choix n°3. Chocolad chaud : 1,50 euros');
+prix.set("1", 1.20);
+prix.set("2", 1.10);
+prix.set("3", 1.50);
 
-if(qteGobelets>=1){
+const boisson = new Map();
+boisson.set("1", 'Café');
+boisson.set("2", 'Thé');
+boisson.set("3", 'Chocolat chaud');
+
+
+
+while(qteGobelets>=1){
 
 let response = await sc.question('Composez le code de votre boisson: \n1 : Café \n2 : Thé  \n3 : Chocolad chaud'); 
 console.log(response);
+
+        if(prix.has(response)&&boisson.has(response)){
+        montant = prix.get(response);
+        choixBoisson= boisson.get(response);
+        let argent = await sc.question('Vous avez sélectionné : '+choixBoisson+'\nVeuillez introduire le montant : '+montant+' euros');
+        console.log(argent);
         
-        if(prix.has(response)){
-        produit = prix.get(response);
-        console.log('Vous avez sélectionné : '+produit);
+
+                if(argent>montant){
+                let rendu=argent-montant;
+                const monnaie = rendu.toFixed(2);
+                console.log('Voici votre monnaie: '+monnaie+' euros')
+                console.log('Boisson en cours de préparation. \nMerci de votre visite.\n') 
+                                  }
+                else if(argent==montant){
+                console.log('Boisson en cours de préparation. \nMerci de votre visite.\n')
+                                        }
+                else{
+                let rendu=argent-montant;
+                const monnaie = rendu.toFixed(2);  
+                console.log('Montant insuffisant, merci d\'introduire le montant nécessaire.')
+                ('Il manque :'+monnaie+'euros')
+                break;
+                    }
         
-                              }
+        
+        
 
 
 
+        qteGobelets = qteGobelets-1;   
+                                                        }
 
-
-
-
-
-
-
-                
         else{
         console.log("Code inexistant. Merci de composer votre code boisson.")
         }
 
-                }
-else{
+                        }
+
 console.log('Machine en attente de ravitaillement de gobelets. Merci de votre compréhension.');      
-    }      
-
-
-
-
-
+         
 
                    
 sc.close();
-                    }
+                        }
 await main()
