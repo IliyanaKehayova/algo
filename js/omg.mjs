90acf6679de1104df9e47ec163316d5ccfc63212
Iliyana KEHAYOVA <iliyana.kehayova2407@gmail.com>
2026-06-11 16:43:25 +0200

    exo js

--- js/omg.mjs
+++ js/omg.mjs
@@ -1,22 +1,3 @@
-/*Exercice Algo 4.1
-Vous disposez de la formule de prévision d’investissement appelé Valeur Actualisé Net de 
-l’investissement.
-Valeur actuelle nette VAN
-Mesure de la rentabilité économique actualisée au taux i.
- n
-VAN= - I0 + ∑ (flux nets) x (1+i) –k + VRn x (1+i)-k
- K=1
-Variable i est le taux ‘actualisation : exemple i =0.04 soir 4%
-I0 : est le montant de l’investissement de départ. 
-n : le nombre d’année et fixé à 5 ans en tout 4 ans de cash-flow + 1an pour la vente de la 
-valeur résiduelle VR.
-Cela fait 4 (flux nets) ou cash-flow à saisir par l’utilisateur. 
-VR : est la valeur résiduelle de de revente de l’investissement.
-Question : Dans le cas d’un investissement réalisé donc sur 4 ans avec une VR de revente
-connue la 5ème année vous réaliserez la fonction qui renvoie la VAN calculé par la formule.
-Plus la VAN est élevée plus l’investissement est rentable */
-
-
 import{createInterface} from 'node:readline/promises';
 import{stdin as input, stdout as output} from 'node:process';
 async function main(){
@@ -24,7 +5,7 @@ const sc = new createInterface({input, output});
 
 
 let taux;
-let n=4;
+let n;
 let I;
 let VR;
 let flux=[];
@@ -39,6 +20,9 @@ console.log(taux);
 
 I = parseFloat(await sc.question('Quel est l\'investissement (capital) ?')); 
 console.log(I);
+
+n = parseFloat(await sc.question('Sur combien d\'années souhaitez-vous investir ?')); 
+console.log(n);
 
 for(i=1; i<=n; i++){
 CF = parseFloat(await sc.question('Quel est le cash-flow pour l\'année CF'+(i)));
