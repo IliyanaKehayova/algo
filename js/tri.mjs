4c60cd30d6b8649a187b44c2f3ad65eacdb623f1
Iliyana KEHAYOVA <iliyana.kehayova2407@gmail.com>
2026-06-22 11:08:49 +0200

    exo js

--- js/tri.mjs
+++ js/tri.mjs
@@ -8,19 +8,54 @@ import { stdin as input, stdout as output } from "node:process";
 async function main() {
 const sc = new createInterface({ input, output });
 
-
-
-do
-let number = ParseInt(await sc.question('Veuillez entrer tous les nombres du tableau.'));
-console.log(number);
-
 let numbers = [];
+let newNumbers = [];
+let smallestIndex;
 
-number.push(number);
+do{
+let number = parseInt(await sc.question('Veuillez entrer 5 nombres dans le tableau.'));
+console.log(number);
+numbers.push(number);
+  }
+while(numbers.length<=4);
+
+console.table(numbers);
+
+console.log(Math.min(...numbers));
+smallestIndex = Math.min(...numbers);
+console.log(smallestIndex);
+
+for(let i=0; i<=numbers.length; i++){
+    let indexMin=i;
+    for (let j=i; j<numbers.length; j++){
+        if(numbers[j] <numbers[indexMin]){
+            indexMin=j; 
+                                         }
+                                       }
+
+        if(indexMin !==i){
+            const temp = numbers[i];
+            numbers[i] = numbers[indexMin];
+            numbers[indexMin] = temp;
+                         }                               
+
+                                    }
+                              
+
+console.table(numbers);
+
+/*
+
+newNumbers.push(smallestIndex);
+
+for(let i=0; i<=numbers.length; i++){
+    newNumbers.push(Math.max(numbers[i]));
+
+}
 
 
-
-
+console.table(newNumbers);
+*/
 sc.close();                    
                       }              
 await main();
